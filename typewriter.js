adc018de453ef3d466023efb31ce479ec8d051ce
Merak Bourque <xtremeyou@hotmail.com>
2024-07-20 14:02:34 -0400

    added \n for new line

--- typewriter.js
+++ typewriter.js
@@ -1,9 +1,12 @@
-const sentence = "hello there from lighthouse labs";
+const sentence = "hello there from lighthouse labs \n";
 
 setTimeout(() => {
   for (let i = 0; i < sentence.length; i++) {
     setTimeout(() => {
       process.stdout.write(sentence[i]);
+
     }, 50 * i);
   }
 });
+
+
